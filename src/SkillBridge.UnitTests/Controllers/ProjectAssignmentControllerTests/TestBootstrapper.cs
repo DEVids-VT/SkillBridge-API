23a523f95c52ac9dee09c50775c61ba6941db776
NKSurprise <krasimiriosifov303@gmail.com>
2025-10-29 20:28:33 +0200

    Add unit tests for Companies and ProjectAssignments controllers
    Introduces comprehensive unit tests for CompaniesController and ProjectAssignmentsController, including action, attribute, routing, and authorization tests using MyTested.AspNetCore.Mvc and Moq. Adds supporting fakes, test startup, and bootstrapping for test infrastructure. Also fixes CompaniesController to enable the CompanyScope authorization policy on the Create action.

---
+++ src/SkillBridge.UnitTests/Controllers/ProjectAssignmentControllerTests/TestBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿// TestsBootstrapper.cs
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using MyTested.AspNetCore.Mvc;
+using SkillBridge.Services.ProjectAssignment;
+
+public sealed class TestsBootstrapper
+{
+    public TestsBootstrapper()
+    {
+        MyApplication
+            .StartsFrom<Program>() // or .StartsFrom<Startup>()
+            .WithServices(services =>
+            {
+                var svcMock = new Mock<IProjectAssignmentService>(MockBehavior.Loose);
+                services.AddSingleton<IProjectAssignmentService>(svcMock.Object);
+            });
+    }
+}
---
+++ src/SkillBridge.UnitTests/StartupBootstrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+using MyTested.AspNetCore.Mvc;
+
+// Runs once when the test assembly loads:
+public static class StartupBootstrapper
+{
+    [ModuleInitializer]
+    public static void Init()
+        => MyApplication.StartsFrom<TestStartup>();
+}
